e13837abb30c1109bd39b30eada6f1787a3dade4
CharlesCao0828 <33383746+CharlesCao0828@users.noreply.github.com>
2022-09-09 17:46:26 +0800

    Update inst-py.bat

--- winagent/inst-py.bat
+++ winagent/inst-py.bat
@@ -1,7 +1,7 @@
 set /p bucketname=<C:\bucketname
 
 echo "Downloading files from gcs" > C:\inst-batlog
-call gsutil -m cp gs://%bucketname%/winagent/python-3.10.6-amd64.exe gs://%bucketname%/winagent/conf gs://%bucketname%/winagent/test.py .
+call gsutil -m cp gs://%bucketname%/python-3.10.6-amd64.exe gs://%bucketname%/conf gs://%bucketname%/test.py .
 
 echo "Installing Python 3.10.6" >> C:\inst-batlog
 python-3.10.6-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
