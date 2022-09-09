ebbd8690431cc7c5e13cb5cb0b02b4d7e4228dc7
CharlesCao0828 <33383746+CharlesCao0828@users.noreply.github.com>
2022-09-09 16:47:50 +0800

    Update inst-py.bat

--- winagent/inst-py.bat
+++ winagent/inst-py.bat
@@ -1,12 +1,15 @@
-set /p bucketname=<bucketname
+set /p bucketname=<C:\bucketname
 
+echo "Downloading files from gcs" > C:\inst-batlog
 call gsutil -m cp gs://%bucketname%/python-3.10.6-amd64.exe gs://%bucketname%/test.py gs://%bucketname%/windev.json gs://%bucketname%/exe.py .
 
-echo "Installing Python 3.10.6"
+echo "Installing Python 3.10.6" >> C:\inst-batlog
 python-3.10.6-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+echo "Successfully installed python" >> C:\inst-batlog
 
-echo "Installing bigquery client for python"
+echo "Installing  google-cloud-bigquery and google-cloud-storage " >> C:\inst-batlog
 py -m pip install -U google-cloud-bigquery google-cloud-storage
+echo "Successully installed  google-cloud-bigquery and google-cloud-storage " >> C:\inst-batlog
 
-echo "Patching"
+echo "Patching" >> C:\inst-batlog
 py test.py
